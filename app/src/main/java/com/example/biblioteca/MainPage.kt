e02bafd26bcdeecb066c5a08ac9bb57d3378c72f
juaarcgon <juanmaarcos21@outlook.com>
2021-04-03 13:09:09 +0200

    principio de integracion con google y log out

--- app/src/main/java/com/example/biblioteca/MainPage.kt
+++ app/src/main/java/com/example/biblioteca/MainPage.kt
@@ -2,12 +2,30 @@ package com.example.biblioteca
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
- enum class ProviderType{
-    BASIC
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main_page.*
+
+enum class ProviderType{
+    BASIC,GOOGLE,FACEBOOK,TWITTER
 }
 class MainPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
+
+        // Setup
+        val bundle= intent.extras
+        val email = bundle?.getString("email")
+        setup(email ?: "")
+    }
+    private fun setup(email:String){
+        title = "Biblioteca"
+        emailTextView.text = email
+        logOutbutton.setOnClickListener(){
+            FirebaseAuth.getInstance().signOut()
+            OnBackPressedDispatcher()
+        }
     }
 }
