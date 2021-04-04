35456f1b7dfa894d0d08e71e2a5c445e746c0662
juaarcgon <juanmaarcos21@outlook.com>
2021-04-04 14:36:02 +0200

    Inicio con Google

--- app/src/main/java/com/example/biblioteca/AuthActivity.kt
+++ app/src/main/java/com/example/biblioteca/AuthActivity.kt
@@ -1,14 +1,22 @@
 package com.example.biblioteca
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import kotlinx.android.synthetic.main.activity_auth.*
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class AuthActivity : AppCompatActivity() {
+    private val GOOGLE_SIGN_IN = 100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
@@ -21,7 +29,26 @@ class AuthActivity : AppCompatActivity() {
 
         // setup
         setup()
+        session()
+    }
 
+    override fun onStart() {
+        super.onStart()
+
+        authLayout.visibility = View.VISIBLE
+    }
+
+    private fun session() {
+        val prefs: SharedPreferences = getSharedPreferences(
+            getString(R.string.prefs_file),
+            Context.MODE_PRIVATE
+        )
+        val email = prefs.getString("email", null)
+        val provider = prefs.getString("provider", null)
+        if (email != null && provider != null) {
+            authLayout.visibility = View.INVISIBLE
+            showMainPage(email, ProviderType.valueOf(provider))
+        }
     }
 
     private fun setup() {
@@ -59,6 +86,16 @@ class AuthActivity : AppCompatActivity() {
             }
 
         }
+        googleImageButton.setOnClickListener() {
+            // Configuracion
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+            googleClient.signOut()
+
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
+        }
+
     }
 
     // alerta si falla
@@ -79,6 +116,34 @@ class AuthActivity : AppCompatActivity() {
         }
         startActivity(mainPageIntent)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GOOGLE_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                if (account != null) {
+
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+                    FirebaseAuth.getInstance().signInWithCredential(credential)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                showMainPage(account.email ?: "", ProviderType.GOOGLE)
+                            } else {
+                                showAlert()
+                            }
+                        }
+
+
+                }
+            }catch (e:ApiException){
+                showAlert()
+        }
+    }
+}
 
 }
 
--- app/src/main/java/com/example/biblioteca/MainActivity.kt
+++ app/src/main/java/com/example/biblioteca/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.biblioteca
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.OnBackPressedDispatcher
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main_page.*
 
@@ -17,21 +18,40 @@ class MainPage : AppCompatActivity() {
 
         // Setup
         val bundle= intent.extras
+        val email = bundle?.getString("email")
+        val provider = bundle?.getString("provider")
         val conf = bundle?.getString("Config")
         setup()
+
+        // guardado de datos
+        val prefs: SharedPreferences.Editor = getSharedPreferences(getString(R.string.prefs_file),Context.MODE_PRIVATE).edit()
+        prefs.putString("email",email)
+        prefs.putString("provider",provider)
+        prefs.apply()
     }
     private fun setup(){
         title = "Biblioteca"
-        configButton.setOnClickListener(){}
-        logOutButton.setOnClickListener(){
-            FirebaseAuth.getInstance().signOut()
-            OnBackPressedDispatcher()
+        configButton.setOnClickListener(){
             backToLogin()
+        }
+        logOutButton.setOnClickListener(){
+
+            // Borrado de datos
+            val prefs: SharedPreferences.Editor = getSharedPreferences(getString(R.string.prefs_file),Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
+            // desconexion base de datos
+            FirebaseAuth.getInstance().signOut()
+
+            // vuelta atras
+            onBackPressed()
+
         }
     }
 
-    // volver al login
-    private fun backToLogin() {
+    // cambiar a configuracion
+     private fun backToLogin() {
         val authPageIntent = Intent(this, AuthActivity::class.java).apply {}
         startActivity(authPageIntent)
     }
